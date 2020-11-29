e44511de87c72e2bd3aec84f20d7a3eed723a379
ConfuzzedCat <73724128+ConfuzzedCat@users.noreply.github.com>
2020-11-29 12:09:08 +0100

    Done with the output

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace tutorialTesting
 {
@@ -6,8 +6,16 @@ namespace tutorialTesting
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Cat", 1000);
+            
+            var account = new BankAccount("Cat", 3500);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} inintial balance. ");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            account.MakeDeposit(356, DateTime.Now, "Got paid for work");
+            account.MakeWithdrawal(1000, DateTime.Now, "Taxes");
+            Console.WriteLine(account.GetAccountHistory());
+
         }
     }
 }
